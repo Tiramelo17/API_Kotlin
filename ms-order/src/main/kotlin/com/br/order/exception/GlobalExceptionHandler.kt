c7d1d04bfab4003a7d7258bca2cfdc43d6cf846b
Gabriel Melo <meloferreiragabriel17@gmail.com>
2025-02-20 11:14:51 -0300

    exepction handler

--- ms-order/src/main/kotlin/com/br/order/exception/GlobalExceptionHandler.kt
+++ ms-order/src/main/kotlin/com/br/order/exception/GlobalExceptionHandler.kt
@@ -8,14 +8,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
 
 @ControllerAdvice
-class GlobalExeptionHandler {
+class GlobalExceptionHandler {
 
     @ExceptionHandler(StatusRuntimeException::class)
-    fun handleNoSuchElementException(ex: StatusRuntimeException, request: WebRequest): ResponseEntity<GlobalErrorResponse> {
+    fun handleStatusRuntimeException(ex: StatusRuntimeException, request: WebRequest): ResponseEntity<GlobalErrorResponse> {
         val errorResponse = GlobalErrorResponse(
             status = ex.status.code.value(),
             message = ex.message ?: ""
         )
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse)
+    }
+
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNoSuchElementException(ex: NoSuchElementException, request: WebRequest): ResponseEntity<GlobalErrorResponse> {
+        val errorResponse = GlobalErrorResponse(
+            status = HttpStatus.NOT_FOUND.value(),
+            message = ex.message ?: ""
+        )
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse)
     }
 }
